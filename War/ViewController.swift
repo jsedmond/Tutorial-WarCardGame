70b5466e7611bab68a86f51a0aa4da77e5a740e3
Jacob Edmond <jacobedmond@Jacobs-MacBook-Pro.local>
2017-05-04 22:37:21 -0500

    initial commit

---
+++ War/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Jacob Edmond on 3/1/17.
+//  Copyright © 2017 Jacob Edmond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0
+    
+    
+    let cardNames = ["card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func dealTapped(_ sender: AnyObject) {
+        
+        // andomize left number from 0 to 12
+        let leftNumber = Int(arc4random_uniform(13))
+        
+        // Set left image
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+        
+        // Randomize right number from 0 to 12
+        let rightNumber = Int(arc4random_uniform(13))
+        
+        // Set right image
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        
+        // Compare the card numbers
+        if leftNumber > rightNumber {
+            // Left card wins
+            
+            // Increment the score
+            leftScore += 1
+            
+            // Update the label
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber == rightNumber {
+            // It's a tie
+        }
+        else {
+            // Right card wins
+            
+            // Increment the score
+            rightScore += 1
+            
+            // Update the label
+            rightScoreLabel.text = String(rightScore)
+        }
+    }
+}
+
